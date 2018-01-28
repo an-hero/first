947ad8401015a39df180d25b193006835a75a084
Admin <001anhero@gmail.com>
2018-01-28 20:03:32 +0300

    create file HelloWorld1

---
+++ src/com/xz/HelloWorld1.java
@@ -0,0 +1,10 @@
+package com.xz;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
